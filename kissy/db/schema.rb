d16b8339bf5680f7fae52bbf682c4fb40af2cccf
Michael <mjames.ayres@gmail.com>
2016-11-30 14:18:33 -0500

    Ran migrations adding attribute to category model

--- kissy/db/schema.rb
+++ kissy/db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130190717) do
+ActiveRecord::Schema.define(version: 20161130191356) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "kisses", force: :cascade do |t|
