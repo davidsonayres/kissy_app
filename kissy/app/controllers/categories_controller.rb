e0e7fd6abb561519b1e799ff632c3ca4cb0adcba
Michael <mjames.ayres@gmail.com>
2016-11-30 15:01:05 -0500

    End of session commit - fiddling with create method in category controller, looking for reason why seeds aren't populating

--- kissy/app/controllers/categories_controller.rb
+++ kissy/app/controllers/categories_controller.rb
@@ -9,7 +9,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.build(category_params)
+    @category = Category.new(category_params)
 
     if @category.save
       redirect_to root
