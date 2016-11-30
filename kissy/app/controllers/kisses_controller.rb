9f63e01551590527c323b890ed8a34ebdaaebc8e
Michael <mjames.ayres@gmail.com>
2016-11-30 13:27:18 -0500

    Added index, new, create, and show methods to kisses controller

--- kissy/app/controllers/kisses_controller.rb
+++ kissy/app/controllers/kisses_controller.rb
@@ -1,2 +1,23 @@
 class KissesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @kiss = Kiss.new
+  end
+
+  def create
+    @kiss = Kiss.new(kiss_params)
+  end
+
+  def show
+    @kiss = Kiss.find(params[:id])
+  end
+
+private
+  def kiss_params
+    params.require(:kiss).permit(:content)
+  end
+  
 end
