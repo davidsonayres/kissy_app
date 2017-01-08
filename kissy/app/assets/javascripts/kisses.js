399a05036c7164dcdc9d20df6e8c515b8ab18a65
Michael <mjames.ayres@gmail.com>
2017-01-07 20:40:02 -0500

    Styled kiss generating button on kiss page, styles categories button in layout

--- kissy/app/assets/javascripts/kisses.js
+++ kissy/app/assets/javascripts/kisses.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('button#kiss_generator').on('click', function(){
+  $('#kiss_generator').on('click', function(){
     $.ajax({
       url: 'http://localhost:3000/categories/1/random_kiss',
       method: 'GET',
