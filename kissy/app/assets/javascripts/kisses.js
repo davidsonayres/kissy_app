06c0d6c23dead3a2d386f246e5c4823b6b359d10
Michael <mjames.ayres@gmail.com>
2017-02-13 15:12:11 -0500

    Added audio kiss files, moved js for kiss generator to kiss index page

--- kissy/app/assets/javascripts/kisses.js
+++ kissy/app/assets/javascripts/kisses.js
@@ -1,12 +1,12 @@
-$(function(){
-  $('#kiss_generator').on('click', function(){
-    $.ajax({
-      url: 'random_kiss',
-      method: 'GET',
-      dataType: 'html'
-    }).done(function(responseData){
-      console.log(responseData);
-      $('div#kiss p').html(responseData);
-    });
-  });
-})
+// $(function(){
+//   $('#kiss_generator').on('click', function(){
+//     $.ajax({
+//       url: 'random_kiss',
+//       method: 'GET',
+//       dataType: 'html'
+//     }).done(function(responseData){
+//       console.log(responseData);
+//       $('div#kiss p').html(responseData);
+//     });
+//   });
+// })
